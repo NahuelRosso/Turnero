79f43eb85c64894f7229e420a455c433a89ea976
Nahuel Rosso <nahuelgrosso25@gmail.com>
2023-05-15 21:33:24 -0300

    codgo limpio, Login y REgister

--- frotn-react/src/Routes.tsx
+++ frotn-react/src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { FormRegister } from "./pages/formRegister/formRegister";
+import { Login } from "./pages/formLogin/formLogin";
 
 
 export const AppRouter = () => {
