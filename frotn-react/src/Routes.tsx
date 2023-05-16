d4db8330e1e16b359eb2b4b1b0686bd0c3c6cfc9
Nahuel Rosso <nahuelgrosso25@gmail.com>
2023-05-15 21:27:54 -0300

    Formulario de Registro

--- frotn-react/src/Routes.tsx
+++ frotn-react/src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import FormRegister from "./pages/formRegister/formRegister";
+import { FormRegister } from "./pages/formRegister/formRegister";
+
 
 export const AppRouter = () => {
     return (
