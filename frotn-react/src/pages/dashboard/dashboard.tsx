97f4a6af2e1ef0a57da06a2e813864e58d1148ce
FacuAllende <tympo.99@gmail.com>
2023-09-18 00:11:57 -0300

    Crear clase Navbar y Drawer, Hacer navbar.

--- frotn-react/src/pages/dashboard/dashboard.tsx
+++ frotn-react/src/pages/dashboard/dashboard.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import NavBar from '../../shared/Components/NavBar/NavBar'
+import { Typography } from '@mui/material'
+
+
 export default function Dashboard() {
-  return (
-    <div>Logueado correctamente</div>
+  return(
+    <div>
+      <NavBar></NavBar>
+    <Typography>
+      Hola aqui estar el dashboard con sus botones
+    </Typography>
+    </div>
+    
   )
 }
